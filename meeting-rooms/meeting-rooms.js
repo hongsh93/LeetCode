28f123fa4d4f71210f846158327d89000591f436
hongsh93 <22565656+hongsh93@users.noreply.github.com>
2021-12-09 14:48:27 +0900

    Time: 72 ms (97.51%), Space: 40.6 MB (74.46%) - LeetHub

---
+++ meeting-rooms/meeting-rooms.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function(intervals) {
+    intervals.sort((a,b) => a[0] - b[0])
+    
+    let res = true
+    for (let i=0; i<intervals.length-1; i++) {
+        if (intervals[i][1] > intervals[i+1][0]) {
+            res = false
+            break
+        }
+    }
+    return res
+};
