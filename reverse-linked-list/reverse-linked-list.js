5c1e8741e2ec2a4c279dc651a071badcbf940819
hongsh93 <22565656+hongsh93@users.noreply.github.com>
2021-12-10 05:33:12 +0900

    Time: 72 ms (93.53%), Space: 40.2 MB (95.12%) - LeetHub

--- reverse-linked-list/reverse-linked-list.js
+++ reverse-linked-list/reverse-linked-list.js
@@ -9,30 +9,65 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
 var reverseList = function(head) {
-    let res_list = []
-    let head_node = head
-    while (head_node) {
-        res_list.push(head_node.val)
-        head_node = head_node.next
+    let curr = head
+    let reverse = null
+    while (head) {
+        let temp = reverse
+        reverse = new ListNode(head.val)
+        reverse.next = temp
+        head = head.next
     }
+    return reverse
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var reverseList = function(head) {
+//     let res_list = []
+//     let head_node = head
+//     while (head_node) {
+//         res_list.push(head_node.val)
+//         head_node = head_node.next
+//     }
     
-    res_list.reverse()
+//     res_list.reverse()
     
-    let res = new ListNode()
-    let res_node = res
-    for (let i=0; i<res_list.length; i++) {
-        res_node.val = res_list[i]
-        if (i !== res_list.length-1) {
-            res_node.next = new ListNode()
-            res_node = res_node.next
-        }
-    }
+//     let res = new ListNode()
+//     let res_node = res
+//     for (let i=0; i<res_list.length; i++) {
+//         res_node.val = res_list[i]
+//         if (i !== res_list.length-1) {
+//             res_node.next = new ListNode()
+//             res_node = res_node.next
+//         }
+//     }
     
-    if (res_list.length === 0) {
-        return null
-    }
-    else {
-        return res
-    }
-};
+//     if (res_list.length === 0) {
+//         return null
+//     }
+//     else {
+//         return res
+//     }
+// };
