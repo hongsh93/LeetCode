d552508fdb9d58452dc5b7f291f62bae93e56c8a
hongsh93 <22565656+hongsh93@users.noreply.github.com>
2021-12-11 20:56:26 +0900

    Time: 112 ms (63.64%), Space: 48.6 MB (40.91%) - LeetHub

---
+++ generalized-abbreviation/generalized-abbreviation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} word
+ * @return {string[]}
+ */
+var generateAbbreviations = function(word) {
+    let res = ["1", word[0]]
+    for (let i=1; i<word.length; i++) {
+        let temp = []
+        for (let j=0; j<res.length; j++) {
+            let a = res[j].slice(-1)
+            if (isNaN(parseInt(a))) {
+                temp.push(res[j]+word[i])
+                temp.push(res[j]+"1")
+            }
+            else {
+                temp.push(res[j]+word[i])
+                temp.push(res[j].slice(0,-1) + (parseInt(a)+1))
+            }
+        }
+        res = temp
+    }
+    return res
+};
