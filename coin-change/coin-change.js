2958af2640fb03beeda6bf6ed2e9c96c2b5a70d6
hongsh93 <22565656+hongsh93@users.noreply.github.com>
2021-12-12 19:12:41 +0900

    Time: 124 ms (68.76%), Space: 44.9 MB (38.93%) - LeetHub

---
+++ coin-change/coin-change.js
@@ -0,0 +1,77 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+
+var coinChange = function(coins, amount) {
+    if (amount === 0) {
+        return 0
+    }
+    let dp = Array(amount+1).fill(0)
+    
+    for (let coin of coins) {
+        if (coin <= amount) {
+            dp[coin] = 1
+        }
+    }
+    for (let i=1; i<dp.length; i++) {
+        for (let coin of coins) {
+            if (dp[i] > 0) {
+                if (i>coin && dp[i-coin]>0) {
+                    dp[i] = Math.min(dp[i], dp[i-coin]+1)
+                }
+            }
+            else {
+                if (i>coin && dp[i-coin]>0) {
+                    dp[i] = dp[i-coin]+1
+                }
+            }
+        }
+    }
+    return dp[amount] > 0 ? dp[amount] : -1
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var coinChange = function(coins, amount) {
+//     if (amount === 0){
+//         return 0
+//     }
+//     let dp = Array(amount+1).fill(0)
+//     // let try = 1
+    
+//     const change = (i) => {
+//         let nextTry = []
+//         for (let j of i) {
+//             for (let coin of coins) {
+//                 if (dp[j+coin] > dp[j]+1 || dp[j+coin] === 0) {
+//                     dp[j+coin] = dp[j]+1
+//                     if (j+coin < amount) {
+//                         nextTry.push(j+coin)
+//                     }
+//                     else if (j+coin === amount) {
+//                         return
+//                     }
+//                 }
+//             }
+//         }
+//         nextTry.length > 0 && change(nextTry)
+//     }
+//     change([0])
+//     return dp[amount] > 0 ? dp[amount] : -1
+// };
