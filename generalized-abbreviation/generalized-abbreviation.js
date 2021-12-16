f0facce065d767d4bb84f3115eb1e869efd67718
hongsh93 <22565656+hongsh93@users.noreply.github.com>
2021-12-16 20:42:39 +0900

    Time: 235 ms (28.57%), Space: 48.7 MB (39.29%) - LeetHub

--- generalized-abbreviation/generalized-abbreviation.js
+++ generalized-abbreviation/generalized-abbreviation.js
@@ -3,21 +3,84 @@
  * @return {string[]}
  */
 var generateAbbreviations = function(word) {
-    let res = ["1", word[0]]
-    for (let i=1; i<word.length; i++) {
+    let res = [""]
+    for (let i=0; i<word.length; i++) {
         let temp = []
         for (let j=0; j<res.length; j++) {
-            let a = res[j].slice(-1)
-            if (isNaN(parseInt(a))) {
+            let lastChar = res[j][res[j].length-1]
+            if (isNaN(lastChar)) {
                 temp.push(res[j]+word[i])
                 temp.push(res[j]+"1")
             }
             else {
                 temp.push(res[j]+word[i])
-                temp.push(res[j].slice(0,-1) + (parseInt(a)+1))
+                temp.push(res[j].slice(0,-1) + (parseInt(lastChar)+1))
             }
         }
         res = temp
     }
     return res
-};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var generateAbbreviations = function(word) {
+//     let res = ["1", word[0]]
+//     for (let i=1; i<word.length; i++) {
+//         let temp = []
+//         for (let j=0; j<res.length; j++) {
+//             let a = res[j].slice(-1)
+//             if (isNaN(parseInt(a))) {
+//                 temp.push(res[j]+word[i])
+//                 temp.push(res[j]+"1")
+//             }
+//             else {
+//                 temp.push(res[j]+word[i])
+//                 temp.push(res[j].slice(0,-1) + (parseInt(a)+1))
+//             }
+//         }
+//         res = temp
+//     }
+//     return res
+// };
