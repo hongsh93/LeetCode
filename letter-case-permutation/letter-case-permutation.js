8e9ba7b83f8472d336640961da55ddbf779587ad
hongsh93 <22565656+hongsh93@users.noreply.github.com>
2021-12-16 20:31:20 +0900

    Time: 133 ms (17.69%), Space: 42.5 MB (84.30%) - LeetHub

--- letter-case-permutation/letter-case-permutation.js
+++ letter-case-permutation/letter-case-permutation.js
@@ -2,24 +2,75 @@
  * @param {string} s
  * @return {string[]}
  */
+
 var letterCasePermutation = function(s) {
     let res = [""]
     for (let i=0; i<s.length; i++) {
         if (isNaN(s[i])) {
-            let temp = []
-            for (let j=0; j<res.length; j++) {
-                temp.push(res[j]+s[i].toUpperCase())
-                temp.push(res[j]+s[i].toLowerCase())                
-            }
-            res = temp
+            res = res.map((a) => a+s[i].toLowerCase()).concat(res.map((a) => a+s[i].toUpperCase()))
         }
         else {
-            let temp = []
-            for (let j=0; j<res.length; j++) {
-                temp.push(res[j]+s[i])     
-            }
-            res = temp
+            res = res.map((a) => a+s[i])
         }
     }
     return res
-};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var letterCasePermutation = function(s) {
+//     let res = [""]
+//     for (let i=0; i<s.length; i++) {
+//         if (isNaN(s[i])) {
+//             let temp = []
+//             for (let j=0; j<res.length; j++) {
+//                 temp.push(res[j]+s[i].toUpperCase())
+//                 temp.push(res[j]+s[i].toLowerCase())                
+//             }
+//             res = temp
+//         }
+//         else {
+//             let temp = []
+//             for (let j=0; j<res.length; j++) {
+//                 temp.push(res[j]+s[i])     
+//             }
+//             res = temp
+//         }
+//     }
+//     return res
+// };
