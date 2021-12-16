d4a0717c5e770b83bed1782bbdad2ca92df65f04
hongsh93 <22565656+hongsh93@users.noreply.github.com>
2021-12-16 15:00:43 +0900

    Time: 142 ms (7.96%), Space: 41.5 MB (41.59%) - LeetHub

---
+++ linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let fast = head
+    let slow = head
+    
+    while (fast && slow) {
+        fast = fast.next
+        if (fast) {
+            fast = fast.next
+        }
+        else {
+            break
+        }
+        slow = slow.next
+        if (fast === slow) {
+            return true
+        }
+    }
+    return false
+};
