868f6180cf0ed04e318cf5e7a25acf452edbfca8
hongsh93 <22565656+hongsh93@users.noreply.github.com>
2021-12-16 16:59:52 +0900

    Time: 104 ms (69.39%), Space: 45.6 MB (53.62%) - LeetHub

--- interval-list-intersections/interval-list-intersections.js
+++ interval-list-intersections/interval-list-intersections.js
@@ -3,24 +3,99 @@
  * @param {number[][]} secondList
  * @return {number[][]}
  */
+
 var intervalIntersection = function(firstList, secondList) {
     let res = []
     let firstIdx = 0
     let secondIdx = 0
     
     while (firstIdx < firstList.length && secondIdx < secondList.length) {
-        let maxStart = Math.max(firstList[firstIdx][0], secondList[secondIdx][0])
-        let minEnd = Math.min(firstList[firstIdx][1], secondList[secondIdx][1])
-        
-        if (minEnd >= maxStart) {
-            res.push([maxStart, minEnd])
+        if (firstList[firstIdx][1] < secondList[secondIdx][0]) {
+            firstIdx++
         }
-        if (firstList[firstIdx][1] > secondList[secondIdx][1]) {
+        else if (secondList[secondIdx][1] < firstList[firstIdx][0]) {
             secondIdx++
         }
+        else if (firstList[firstIdx][0] >= secondList[secondIdx][0]) {
+            if (firstList[firstIdx][1] < secondList[secondIdx][1]) {
+                res.push([firstList[firstIdx][0], firstList[firstIdx][1]])
+                firstIdx++
+            }
+            else {
+                res.push([firstList[firstIdx][0], secondList[secondIdx][1]])
+                secondIdx++
+            }
+        }
         else {
-            firstIdx++
+            if (firstList[firstIdx][1] < secondList[secondIdx][1]) {
+                res.push([secondList[secondIdx][0], firstList[firstIdx][1]])
+                firstIdx++
+            }
+            else {
+                res.push([secondList[secondIdx][0], secondList[secondIdx][1]])
+                secondIdx++
+            }
         }
     }
     return res
-};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var intervalIntersection = function(firstList, secondList) {
+//     let res = []
+//     let firstIdx = 0
+//     let secondIdx = 0
+    
+//     while (firstIdx < firstList.length && secondIdx < secondList.length) {
+//         let maxStart = Math.max(firstList[firstIdx][0], secondList[secondIdx][0])
+//         let minEnd = Math.min(firstList[firstIdx][1], secondList[secondIdx][1])
+        
+//         if (minEnd >= maxStart) {
+//             res.push([maxStart, minEnd])
+//         }
+//         if (firstList[firstIdx][1] > secondList[secondIdx][1]) {
+//             secondIdx++
+//         }
+//         else {
+//             firstIdx++
+//         }
+//     }
+//     return res
+// };
