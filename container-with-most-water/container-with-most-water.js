485c01d993d15a5682ea68d60832c6b7fc0f4497
hongsh93 <22565656+hongsh93@users.noreply.github.com>
2021-12-27 14:43:18 +0900

    Time: 96 ms (33.60%), Space: 48.4 MB (21.33%) - LeetHub

--- container-with-most-water/container-with-most-water.js
+++ container-with-most-water/container-with-most-water.js
@@ -2,44 +2,105 @@
  * @param {number[]} height
  * @return {number}
  */
+
 var maxArea = function(height) {
+    let l = height.length
+    
     let left = 0
-    let right = height.length-1
-    let max = Area(height, left, right)
+    let right = l-1
+    let max = Math.min(height[left], height[right]) * (right-left)
     
-    while (right > left) {
-        let last_left = left
-        while (height[left] <= height[last_left] && right > left) {
-            left += 1
+    while (left<right) {
+        if (height[left] > height[right]) {
+            right--
         }
-        if (left == right) {
-            left = last_left
+        else {
+            left++
         }
-    
-        let last_right = right
-        while (height[right] <= height[last_right] && right > last_left) {
-            right -= 1
-        }
-        if (last_left == right) {
-            right = last_right
-        }
-        
-        max = Math.max(max, Area(height, last_left, right), Area(height, left, last_right), Area(height, left, right))
-        if (max == Area(height, last_left, right) && right != last_right) {
-            left = last_left
-        }
-        else if (max == Area(height, left, last_right) && left != last_left) {
-            right = last_right
-        }
-        // console.info(left, right, max)
-        if (left == last_left && right == last_right) {
-            break;
-        }
+        max = Math.max(max, Math.min(height[left], height[right]) * (right-left))
     }
     
     return max
-};
-
-var Area = function(height, x1, x2) {
-    return Math.min(height[x1], height[x2]) * (x2-x1)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var maxArea = function(height) {
+//     let left = 0
+//     let right = height.length-1
+//     let max = Area(height, left, right)
+    
+//     while (right > left) {
+//         let last_left = left
+//         while (height[left] <= height[last_left] && right > left) {
+//             left += 1
+//         }
+//         if (left == right) {
+//             left = last_left
+//         }
+    
+//         let last_right = right
+//         while (height[right] <= height[last_right] && right > last_left) {
+//             right -= 1
+//         }
+//         if (last_left == right) {
+//             right = last_right
+//         }
+        
+//         max = Math.max(max, Area(height, last_left, right), Area(height, left, last_right), Area(height, left, right))
+//         if (max == Area(height, last_left, right) && right != last_right) {
+//             left = last_left
+//         }
+//         else if (max == Area(height, left, last_right) && left != last_left) {
+//             right = last_right
+//         }
+//         // console.info(left, right, max)
+//         if (left == last_left && right == last_right) {
+//             break;
+//         }
+//     }
+    
+//     return max
+// };
+
+// var Area = function(height, x1, x2) {
+//     return Math.min(height[x1], height[x2]) * (x2-x1)
+// }
