f2d451061fdf8c93bbbd97347d76a6d3350e52cd
hongsh93 <22565656+hongsh93@users.noreply.github.com>
2021-12-27 22:15:55 +0900

    Time: 76 ms (64.85%), Space: 41.3 MB (14.67%) - LeetHub

--- generate-parentheses/generate-parentheses.js
+++ generate-parentheses/generate-parentheses.js
@@ -2,28 +2,92 @@
  * @param {number} n
  * @return {string[]}
  */
+
 var generateParenthesis = function(n) {
-    let res = ["("]
-    let left = 1
+    let res = [""]
+    let left = 0
     let right = 0
     
-    res = makeParenthesis(res, left, right, n)
-    
-    return res
-};
+    return makeParenthesis(res, left, right, n)
+}
 
 var makeParenthesis = function(res, left, right, n) {
-    let temp = []
+    // console.info(res, left, right)
     if (left === n && right === n) {
         return res
     }
-    for (let i=0; i<res.length; i++) {
-        if (left < n) {
-            temp.push(...makeParenthesis([res[i]+"("], left+1, right, n))
-        }
-        if (right < left) {
-            temp.push(...makeParenthesis([res[i]+")"], left, right+1, n))
-        }
+    let temp = []
+    if (left < n) {
+        temp.push(...makeParenthesis(res.map((el) => el+"("), left+1, right, n))
+    }
+    if (left > right) {
+        temp.push(...makeParenthesis(res.map((el) => el+")"), left, right+1, n))
     }
     return temp
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var generateParenthesis = function(n) {
+//     let res = ["("]
+//     let left = 1
+//     let right = 0
+    
+//     res = makeParenthesis(res, left, right, n)
+    
+//     return res
+// };
+
+// var makeParenthesis = function(res, left, right, n) {
+//     let temp = []
+//     if (left === n && right === n) {
+//         return res
+//     }
+//     for (let i=0; i<res.length; i++) {
+//         if (left < n) {
+//             temp.push(...makeParenthesis([res[i]+"("], left+1, right, n))
+//         }
+//         if (right < left) {
+//             temp.push(...makeParenthesis([res[i]+")"], left, right+1, n))
+//         }
+//     }
+//     return temp
+// }
