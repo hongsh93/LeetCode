d33e0206968b0fd07a27f261ce4410e5e5ec37a7
hongsh93 <hongsh930@gmail.com>
2022-01-11 08:20:10 +0900

    Time: 721 ms (7.09%), Space: 44.2 MB (37.01%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.js
@@ -0,0 +1,89 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+var countArrangement = function(n) {
+    let res = 0
+    let nums = Array(n).fill().map((cur, idx) => idx+1)
+    let seen = new Set()
+    
+    const count = function(num) {
+        if (num === n+1) {
+            res++
+            return
+        }
+        for (let i=1; i<=n; i++) {
+            if (!seen.has(i) && (i%num === 0 || num%i === 0)) {
+                seen.add(i)
+                count(num+1)
+                seen.delete(i)
+            }
+        }
+    }
+    
+    count(1)
+    return res
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+// var countArrangement = function(n) {
+//     let visited = Array(n+1).fill(false)
+//     let res = 0
+    
+//     const fillNum = (i) => {
+//         if (i > n) {
+//             res++
+//             return
+//         }
+//         for (let j=1; j<=n; j++) {
+//             if (visited[j] === false && (i%j === 0 || j%i === 0)) {
+//                 visited[j] = true
+//                 fillNum(i+1)
+//                 visited[j] = false
+//             }
+//         }
+//     }
+//     fillNum(1)
+//     return res
+// };
