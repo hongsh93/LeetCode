0e7aa27329a36ddc6f115eb858c182a2a8ba3276
hongsh93 <hongsh930@gmail.com>
2022-01-11 06:36:25 +0900

    Time: 128 ms (30.12%), Space: 42.2 MB (61.45%) - LeetHub

---
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.js
@@ -0,0 +1,75 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves = function(nums) {
+    let min = Math.min(...nums)
+    let res = 0
+    for (let i=0; i<nums.length; i++) {
+        res += (nums[i]-min)
+    }
+    return res
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var minMoves = function(nums) {
+//     let min = nums[0]
+//     let ans = 0
+//     for (let i=1; i<nums.length; i++) {
+//         if (nums[i] < min) {
+//             let temp = min-nums[i]
+//             min = nums[i]
+//             ans += (temp * i)
+//         }
+//         else {
+//             ans += (nums[i]-min)
+//         }
+//     }
+//     return ans
+// };
